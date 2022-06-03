fb1272576344038234b3e5f5976b03da07260a60
Felipe Ivanaga <felipeivanaga@pm.me>
2022-06-03 13:45:23 -0300

    feat: ensure to call usersService with correct values

--- api/src/users/users.controller.spec.ts
+++ api/src/users/users.controller.spec.ts
@@ -1,18 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let sut: UsersController;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [UsersService, PrismaService],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    sut = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
+
+    usersService.create = jest.fn().mockResolvedValue({});
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(sut).toBeDefined();
+  });
+
+  describe('create', () => {
+    const newUserData: CreateUserDto = {
+      name: 'any_name',
+      email: 'any_email@email.com',
+      password: 'any_password',
+    };
+
+    it('should call usersService with correct values', async () => {
+      sut.create(newUserData);
+      expect(usersService.create).toBeCalledTimes(1);
+      expect(usersService.create).toBeCalledWith(newUserData);
+    });
   });
 });
