9b37bb58215428af38b6601376c8445b8b3c81ce
Felipe Ivanaga <felipeivanaga@pm.me>
2022-06-03 06:13:53 -0300

    feat: ensure prisma is called with correct values

--- api/src/users/users.service.spec.ts
+++ api/src/users/users.service.spec.ts
@@ -1,18 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let sut: UsersService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, PrismaService],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    sut = module.get<UsersService>(UsersService);
+    prisma = module.get<PrismaService>(PrismaService);
+
+    prisma.user.create = jest.fn().mockResolvedValue({});
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(sut).toBeDefined();
+  });
+
+  describe('create', () => {
+    const uuidRegex =
+      /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/;
+
+    it('should call prisma with correct values', async () => {
+      const newUserData: CreateUserDto = {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+      };
+
+      await sut.create(newUserData);
+
+      expect(prisma.user.create).toHaveBeenCalledTimes(1);
+      expect(prisma.user.create).toHaveBeenCalledWith({
+        data: {
+          id: expect.stringMatching(uuidRegex),
+          name: newUserData.name,
+          email: newUserData.email,
+          hash: expect.not.stringContaining(newUserData.password),
+        },
+      });
+    });
   });
 });
--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { hash as generateHash } from 'bcrypt';
+import { v4 as generateUuid } from 'uuid';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async create({ name, email, password }: CreateUserDto) {
+    const id = generateUuid();
+    const hash = await generateHash(password, 10);
+
+    this.prisma.user.create({
+      data: {
+        id,
+        name,
+        email,
+        hash,
+      },
+    });
+  }
+}
